6d24dd40b297d31674d9476863826b25ad002b8b
Gabriel Santos <jogabriel.professional@gmail.com>
2023-03-31 19:40:10 -0300

    Adding filters

---
+++ client/src/components/Filters.js
@@ -0,0 +1,59 @@
+import React, { useRef } from "react";
+
+import data from "../util/dataFilters";
+
+function Filters() {
+    const sectionRef = useRef(null);
+
+    const handleNavigation = (direction) => {
+        const section = sectionRef.current;
+        const scrollAmount = 150;
+
+        if (direction === "left") {
+            section.scroll({
+                left: section.scrollLeft - scrollAmount,
+                behavior: "smooth"
+            });
+
+        } else if (direction === "right") {
+            section.scroll({
+                left: section.scrollLeft + scrollAmount,
+                behavior: "smooth"
+            });
+        }
+    };
+
+    return (
+        <div className="section-filters-container">
+            <div className="navigation-buttons">
+                <button onClick={() => handleNavigation("left")}>
+                    <img src={require("../util/images/left-arrow.png")} alt=""/>
+                </button>
+            </div>
+
+            <section className="section-filters" ref={sectionRef}>
+                {
+                    data.map((item, index) => (
+                        <div className="filter" key={index}>
+                            <img src={item.imageSrc} alt={item.description}/>
+                            <h4 className="filter--description">{item.description}</h4>
+                        </div>
+                    ))
+                }
+            </section>
+
+            <div className="navigation-buttons">
+                <button onClick={() => handleNavigation("right")}>
+                    <img src={require("../util/images/right-arrow.png")} alt=""/>
+                </button>
+            </div>
+
+            <div className="button-filters">
+                <img src={require("../util/images/filters.png")} alt=""/>
+                <h4 className="button-filters--description">Filters</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Filters;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,33 +1,40 @@
 import React from "react";
 
+import Filters from "./Filters";
+
 function Navbar() {
 
     return (
-        <header className="header">
-            <a href="">
-                <img className="logo" src={require("../util/images/logo.png")} alt=""/>
-            </a>
+        <React.Fragment>
+            <header className="header">
+                <a href="">
+                    <img className="logo" src={require("../util/images/logo.png")} alt=""/>
+                </a>
 
-            <div className="header-options">
-                <h4 className="header-options--text">Anywhere</h4>
-                <h4 className="header-options--text border">Any week</h4>
-                <h4 className="header-options--text fw-400">Add guests</h4>
+                <div className="header-options">
+                    <h4 className="header-options--text">Anywhere</h4>
+                    <h4 className="header-options--text border">Any week</h4>
+                    <h4 className="header-options--text fw-400">Add guests</h4>
 
-                <div className="header-options--search">
-                    <img src={require("../util/images/search.png")} alt=""/>
+                    <div className="header-options--search">
+                        <img src={require("../util/images/search.png")} alt=""/>
+                    </div>
                 </div>
-            </div>
 
-            <div className="header-user">
-                <h4 className="header-user--home">Airbnb your home</h4>
-                <img className="header-user--region" src={require("../util/images/region.png")} alt=""/>
+                <div className="header-user">
+                    <h4 className="header-user--home">Airbnb your home</h4>
+                    <img className="header-user--region" src={require("../util/images/region.png")} alt=""/>
 
-                <div className="header-user--info">
-                    <img className="header-user--menu" src={require("../util/images/menu.png")} alt=""/>
-                    <img className="header-user--profile" src={require("../util/images/profile.png")} alt=""/>
+                    <div className="header-user--info">
+                        <img className="header-user--menu" src={require("../util/images/menu.png")} alt=""/>
+                        <img className="header-user--profile" src={require("../util/images/profile.png")} alt=""/>
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+
+            <Filters/>
+
+        </React.Fragment>
     )
 }
 
