0a6febd965ed06621326ab2593246b83c8b66b5a
Gabriel Santos <jogabriel.professional@gmail.com>
2023-04-02 17:08:12 -0300

    Implementing Room page

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -4,7 +4,34 @@ function Room() {
 
     return (
         <section className="section-room">
-            <h2>Room</h2>
+            <h1 className="room-title">The Valley Lodge: Stunning view and cosiness!</h1>
+
+            <div className="room--info-share-save">
+                <div className="room-info">
+                    <div className="card-room--rating">
+                        <img src={require("../util/images/star.png")} alt=""/>
+                        <h4 className="rating">5.0</h4>
+                    </div>
+
+                    <img src={require("../util/images/dot.png")} alt=""/>
+                    <h2 className="room-info--reviews">40 reviews</h2>
+                    <img src={require("../util/images/dot.png")} alt=""/>
+                    <h2 className="room-info--location">Alfredo Chaves, State of Espírito Santo, Brazil</h2>
+                </div>
+
+                <div className="share-save">
+                    <div className="share">
+                        <img src={require("../util/images/share.png")} alt=""/>
+                        <h2 className="room-info--share">Share</h2>
+                    </div>
+
+                    <div className="save">
+                        <img src={require("../util/images/save.png")} alt=""/>
+                        <h2 className="room-info--save">Save</h2>
+                    </div>
+                </div>
+            </div>
+
         </section>
     )
 }
