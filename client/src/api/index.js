447e92667681cb30b14fd8f16d3ebd74daa11eb5
Gabriel Santos <jogabriel.professional@gmail.com>
2023-04-10 15:44:44 -0300

    Refactoring

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,10 +4,8 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((request) => {
     if (localStorage.getItem('profile')) {
-        request.headers.Authorization = JSON.parse(localStorage.getItem('profile'))['token'].credential ?
-            `Bearer ${JSON.parse(localStorage.getItem('profile'))['token'].credential}` :
-            `Bearer ${JSON.parse(localStorage.getItem('profile'))['token']}`
+        request.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile'))['token'].credential}`;
     }
 
-    return request
+    return request;
 })
