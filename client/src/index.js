db5b57efeedb5eef202820f73320b27783f0a045
Gabriel Santos <jgabriel.professional@gmail.com>
2023-06-01 17:06:50 -0300

    Removing unnecessary Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import { applyMiddleware, compose, createStore } from "redux";
-import { Provider } from "react-redux";
-
-import thunk from "redux-thunk";
-
-import { reducers } from "./reducers";
-
 import "./styles/generic.css"
 import "./styles/queries.css";
 import "./styles/style.css";
 
 import App from "./App";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <Provider store={store}><App/></Provider>
+    <App/>
 );
